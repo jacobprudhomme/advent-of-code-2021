9e65ba605a2e5a939bf38e0d8bfc1853a3cabe9c
Jacob Prud'homme <jacobprudhomme@users.noreply.github.com>
2021-12-01 02:05:08 -0500

    Completed part 2

---
+++ src/day1/part2.rs
@@ -0,0 +1,39 @@
+use std::io::{stdin, Read};
+
+fn process_input(input: &str) -> Vec<u16> {
+    input
+        .lines()
+        .map(|num| num.parse().unwrap())
+        .collect()
+}
+
+fn collect_3_depths(data: Vec<u16>) -> Vec<u16> {
+    data
+        .windows(3)
+        .map(|elem| elem.iter().sum())
+        .collect()
+}
+
+fn compare_depths(data: Vec<u16>) -> u16 {
+    data
+        .windows(2)
+        .fold(0, |acc, elem| {
+            if let [prev_depth, curr_depth] = elem {
+                if curr_depth > prev_depth { acc + 1 } else { acc }
+            } else {
+                acc
+            }
+        })
+}
+
+fn main() {
+    let mut buffer = String::new();
+    stdin()
+        .read_to_string(&mut buffer)
+        .expect("Cannot read input");
+    let data = process_input(&buffer);
+
+    let count = compare_depths(collect_3_depths(data));
+
+    println!("{}", count);
+}
